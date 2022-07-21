3bb54449599dbaf165a81068adc45a15362d398b
Thomas Düren <64553863+thomasD01@users.noreply.github.com>
2022-07-21 12:21:10 +0000

    switch back to nextjs

---
+++ server/index.ts
@@ -0,0 +1,34 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { parse } from 'url';
+import next from 'next';
+
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = 8433
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
+
+async function bootstrap(){
+  await app.prepare();
+  try{
+    let server = createServer(async (req: IncomingMessage, res: ServerResponse) => {
+      try{
+        const parsedUrl = parse(req.url!, true)
+        const { pathname, query } = parsedUrl
+        
+        await handle(req, res, parsedUrl);
+      }
+      catch(error){
+        //render error page  
+      }
+    })
+    server.listen(port, ()=>{
+      console.log('\x1b[32mready \x1b[0m- started server on url: https://localhost:' + port);
+    })
+  }
+  catch(error){
+    //error creating server
+  }
+}
+bootstrap();
