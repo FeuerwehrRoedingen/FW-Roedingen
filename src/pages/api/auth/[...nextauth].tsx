2333592731a42a4e83b1cddb9ba87614ec1ee5db
Thomas Düren <thomas.dueren@hotmail.de>
2022-09-02 00:50:43 +0200

    fix

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,13 +1,9 @@
 //Libraries
-import ReactDOMServer from 'react-dom/server'
-import NextAuth, { Theme } from "next-auth"
+import NextAuth from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
-import EmailProvider, { SendVerificationRequestParams } from "next-auth/providers/email"
-import { createTransport } from "nodemailer"
 //Files
-import { getUser } from 'server/Database'
-import EMailComponent from 'src/components/EMailComponent'
-import type { User } from 'server/prisma/types'
+import { getUser } from '../../../../server/db/Database'
+import type { User } from '@prisma/client'
 
 export default NextAuth({
   providers: [
@@ -29,32 +25,6 @@ export default NextAuth({
           return resolve(null);
         });
       }
-    }),
-    EmailProvider({
-      server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM,
-      sendVerificationRequest
     })
   ]
 })
-
-async function sendVerificationRequest(params: SendVerificationRequestParams) {
-  const { identifier, provider, theme, url } = params
-  const { host } = new URL(url)
-  const transport = createTransport(provider.server)
-  const result = await transport.sendMail({
-    to: identifier,
-    from: provider.from,
-    subject: `Sign in to ${host}`,
-    text: text({ url, host }),
-    html: ReactDOMServer.renderToStaticMarkup(<EMailComponent url={url} host={host} theme={theme}/>),
-  })
-  const failed = result.rejected.concat(result.pending).filter(Boolean)
-  if (failed.length) {
-    throw new Error(`Email(s) (${failed.join(", ")}) could not be sent`)
-  }
-}
-
-function text({ url, host }: { url: string; host: string }) {
-  return `Sign in to ${host}\n${url}\n\n`
-}
