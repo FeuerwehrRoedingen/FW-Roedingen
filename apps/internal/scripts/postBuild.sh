8aece0eaefc6b4be68ec1055a0d2e5702965fb06
thomasD01 <thomas.dueren@hotmail.de>
2023-04-25 00:57:08 +0200

    add correct postbuild

--- apps/internal/scripts/postBuild.sh
+++ apps/internal/scripts/postBuild.sh
@@ -3,10 +3,5 @@
 # This script is run after the build process is complete.
 # It is run in the context of the build container.
 
-mv .next/standalone/apps/server/internal/.next .next/standalone
-mv -f .next/standalone/apps/server/internal/package.json .next/standalone
-mv .next/standalone/apps/server/internal/server.js .next/standalone
-mv .next/standalone/apps/server/node_modules .next/standalone
-
-rm -rf .next/standalone/apps
-rm -rf .next/standalone/shared
+cp -R public .next/standalone/apps/internal/public
+cp -R .next/static .next/standalone/apps/internal/.next/static
