82b49e6c07c5165e5970959b41544a50cd100d8a
ThomasD01 <thomas.dueren@hotmail.de>
2023-05-07 15:25:01 +0200

    .

--- apps/web/src/pages/impressum/kontakt.tsx
+++ apps/web/src/pages/impressum/kontakt.tsx
@@ -2,28 +2,25 @@ import React, { Component } from 'react'
 import HomeButton from '../../components/HomeButton';
 
 type Props = {}
-type State = {}
 
-export default class Kontakt extends Component<Props, State> {
-  state = {}
+export default function (props: Props) {
 
-  render() {
-    const to='info@feuerwehr-roedingen.de';
-    const subject='Webseite'
-    const body='\n\n\n Gesendet durch den Email Link der Webseite'
-    return (
-      <div className='bg-gray-900 h-screen px-4 py-4 text-silver'>
-        <HomeButton/>
-        <div className='mt-10'>
-          Sie können uns per Email erreichen unter {to}<br/>
-        </div>
-        <a 
-          className='text-ral-3020 ml-2'
-          href={`mailto:${to}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`}
-        >
-          Email senden
-        </a>
+  const to = 'info@feuerwehr-roedingen.de';
+  const subject = 'Webseite'
+  const body = '\n\n\n Gesendet durch den Email Link der Webseite'
+
+  return (
+    <div className='bg-gray-900 h-screen px-4 py-4 text-silver'>
+      <HomeButton />
+      <div className='mt-10'>
+        Sie können uns per Email erreichen unter {to}<br />
       </div>
-    )
-  }
+      <a
+        className='text-ral-3020 ml-2'
+        href={`mailto:${to}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`}
+      >
+        Email senden
+      </a>
+    </div>
+  )
 }
