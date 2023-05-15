3f5ffeb9e7a8b1e094bef7ea8cfcc3de33293f8c
thomasD01 <thomas.dueren@hotmail.de>
2023-05-15 04:57:25 +0200

    start work on rest api

--- apps/api/microservices/rest-api/src/main.ts
+++ apps/api/microservices/rest-api/src/main.ts
@@ -1,15 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-      }
-    });
-  await app.listen();
-}
-bootstrap();
+const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+	AppModule,
+  {
+    transport: Transport.TCP,
+    options: {
+      host: '0.0.0.0',
+      port: 3002,
+    },
+  }
+);
+
+await app.init();
+
+app.listen();
